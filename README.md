36e76b7db1cd6b8b78dc43df8aa768ce0a778e7f
Blake Bassett <blkbsstt@gmail.com>
2013-10-04 15:16:10 -0500

    update using pgmagick for balanced margins

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 hotdogbun.py
 ============
 
+This is a fork of the excellent conference paper splitting script, [hotdogbun](https://github.com/cscorley/hotdogbun),
+by [cscorley](https://github.com/cscorley).
+
+
 Let's split some conference papers in half so we can read them on
 smaller devices in continuous scroll mode instead of fumbling around on
 the screen like idiots.
 
 All this script does is split each page of a PDF down the middle,
-hotdog bun style.
+hotdog bun style. **It also tries to determine the bounding box of the
+PDF content to create more balanced margins.**
 
 Usage
 -----
@@ -18,17 +23,14 @@ That's it. Your split pdf will be located at './paper-split.pdf'
 Requirements
 ------------
 
-Right now, this script uses the [PyPDF2](https://github.com/knowah/PyPDF2/)
-library to split things, which requires manual installation. I'd really like
-to change that to something that can be installed from `pip`. Let me know.
+Right now, this script uses the [PyPDF2](https://github.com/mstamy2/PyPDF2/) and [pgmagick](https://pypi.python.org/pypi/pgmagick/) python packages. pgmagick is a wrapper around the GraphicsMagick library which uses the Magick++ API through boost.python. Both required packages can be installed through pip.
 
-For now, you can install the current PyPDF2 by running `make pypdf2`.
 
 ....Hotdog bun?
 ---------------
-`python2 `<a href="http://en.wikipedia.org/wiki/File:Hotdog_-_Evan_Swigart.jpg"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/150px-Hotdog_-_Evan_Swigart.jpg"></a>`.py` <img src="https://raw.github.com/cscorley/hotdogbun/master/orig.png" height=300px>
+`python2 `<a href="http://en.wikipedia.org/wiki/File:Hotdog_-_Evan_Swigart.jpg"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/150px-Hotdog_-_Evan_Swigart.jpg"></a>`.py` <img src="https://raw.github.com/blkbsstt/hotdogbun/master/orig.png" height=300px>
 ***=======>***
-<img src="https://raw.github.com/cscorley/hotdogbun/master/split.png" height=300px>
+<img src="https://raw.github.com/blkbsstt/hotdogbun/master/split.png" height=300px>
 
 
 ... basically.
--- hotdogbun.py
+++ hotdogbun.py
@@ -1,5 +1,6 @@
 import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from pgmagick import Image
 
 def split(paperpdf, splitpdf):
     output = PdfFileWriter()
@@ -14,6 +15,10 @@ def split(paperpdf, splitpdf):
             pagecount = left.getNumPages()
             print("%s has %s pages to split." % (paperpdf,pagecount))
 
+            images = [Image(paperpdf + "[%d]" % i) for i in range(0, pagecount)]
+            left_margin, left_gutter, right_margin, right_gutter = get_average_margins(images)
+            gutter_width = left_gutter + right_gutter
+
             for num in range(0, pagecount):
                 left_page = left.getPage(num)
                 right_page = right.getPage(num)
@@ -22,15 +27,52 @@ def split(paperpdf, splitpdf):
                         left_page.mediaBox.getUpperRight_y()
                         )
 
-                left_page.mediaBox.upperRight = midpoint
+                left_page.mediaBox.upperRight = (midpoint[0] + left_gutter, midpoint[1])
+                left_page.mediaBox.upperLeft = (left_margin - gutter_width, midpoint[1])
                 output.addPage(left_page)
 
-                right_page.mediaBox.upperLeft = midpoint
+                right_page.mediaBox.upperRight = (right_page.mediaBox.getWidth() - right_margin + gutter_width , midpoint[1])
+                right_page.mediaBox.upperLeft = (midpoint[0] - right_gutter, midpoint[1])
                 output.addPage(right_page)
 
             print("Writing %s pages to %s" % (output.getNumPages(), splitpdf))
             with open(splitpdf, "wb") as s:
                 output.write(s)
+
+def avg(l):
+    if len(l) > 0:
+        return sum(l)/len(l)
+    else:
+        return 0;
+
+def get_margins(image):
+    return image.boundingBox().xOff(), image.size().width() - (image.boundingBox().xOff() + image.boundingBox().width())
+
+def get_average_margins(images):
+    margins = [get_margins(im) for im in images]
+    left_margin = min([m[0] for m in margins])
+    right_margin = min([m[1] for m in margins])
+
+    left_split = []
+    right_split = []
+
+    for num in range(0, len(images)):
+        page = images[num]
+        geom = page.size()
+        geom.width(geom.width()/2)
+        left_split.append(Image(page))
+        left_split[-1].crop(geom)
+        geom.xOff(geom.width())
+        right_split.append(Image(page))
+        right_split[-1].crop(geom)
+
+    right_gutters = [get_margins(im)[1] for im in left_split]
+    left_gutters = [get_margins(im)[0] for im in right_split]
+
+    right_gutter = avg([m for m in right_gutters if m > 0])
+    left_gutter = avg([m for m in left_gutters if m > 0])
+
+    return left_margin, left_gutter, right_margin, right_gutter
 
 def main(argv):
     # options are for suckers
